7059e33454d5e6a6f2dcf005c384bb6f67ffa7ca
David Felix <david.felix02@hotmail.com>
2020-09-18 01:34:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = tutorials.map((tutorial) => {tutorial[.toUpperCase();
+
+const titleCased = tutorials.map(tutorial => {
+  tutorial[0].toUpperCase();
   return tutorial;
-};
+});
 console.log(titleCased);
