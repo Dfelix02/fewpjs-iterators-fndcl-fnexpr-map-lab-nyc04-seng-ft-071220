b5e3dfa69f2cb1617a6d93ec8350a0718d959e2d
David Felix <david.felix02@hotmail.com>
2020-09-18 02:33:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = tutorials.map((tutorial) => tutorial.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)));
+const titleCased = tutorials.map((tutorial) => tutorial.split(" ").map(word =>{ 
+  word.charAt(0).toUpperCase() + word.slice(1)
+  console.log(word);
+}
+));
 console.log(titleCased)
