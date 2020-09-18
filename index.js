adb8fbb134b0d20fb8dc4ace726d650113967441
David Felix <david.felix02@hotmail.com>
2020-09-18 01:49:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const tutorials = [
 ];
 
 
-let words = words.map(tutorial => {
+let words = tutorials.map(tutorial => {
   let word = tutorial.split(" ");
   tutorial[0].toUpperCase();
   return tutorial;
