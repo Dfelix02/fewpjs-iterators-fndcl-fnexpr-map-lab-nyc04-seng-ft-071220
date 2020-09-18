0a5225e420c55b6955b3ab51b47d5c9825b2b4c9
David Felix <david.felix02@hotmail.com>
2020-09-18 02:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tutorials = [
 
 const titleCased = tutorials.map((tutorial) => tutorial.split(" ").map(word =>{ 
   word.charAt(0).toUpperCase() + word.slice(1)
-  console.log(word);
+  return word;
 }
 ));
 console.log(titleCased)
